f3a9d46b83effc119df94d679633ae19099f6f8b
Ayushi <dalsaniyauma@gmail.com>
2025-04-16 16:14:07 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144
+let accountEmail = "ayushi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+let accountState
+
+console.log(accountId);
+
+accountEmail = "iuc@gmail.com"
+accountPassword = "2345"
+accountCity = "rajkot"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
